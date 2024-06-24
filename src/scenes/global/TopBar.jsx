5ac75b321abdbb0d31c74e631c185f2b4f68e0aa
lmvinh <amis.lmv1304@gmail.com>
2024-06-24 14:42:06 +0700

    Upload sideBar - onProcess

--- src/scenes/global/TopBar.jsx
+++ src/scenes/global/TopBar.jsx
@@ -1,17 +1,16 @@
 import {Box,IconButton,colors,useTheme} from "@mui/material" 
 import { useContext } from "react";
 import { ColorModeContext,tokens } from "../../Theme";
-import InputBase from "@mui/material";
-import { LightModeOutlinedIcon } from "@mui/icons-material/LightModeOutlined";
-import { DarkModeOutlinedIcon } from "@mui/icons-material/DarkModeOutlined";
-import { NotificationsOutlinedIcon } from "@mui/icons-material/NotificationsOutlined";
-import { SettingsOutlinedIcon } from "@mui/icons-material/SettingsOutlined";
-import { PersonOutlinedIcon } from "@mui/icons-material/PersonOutlined";
-import { SearchIcon } from "@mui/icons-material/Search";
-
+import {InputBase} from "@mui/material/";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchIcon from "@mui/icons-material/Search";
 const TopBar = () => {
     const theme = useTheme();
-    const color = tokens(theme.palette.mode);
+    const colors = tokens(theme.palette.mode);
     const colorMode = useContext(ColorModeContext);
     return <Box display="flex" justifyContent="space-between" p = {2}>
         <Box display = "flex"
@@ -25,18 +24,18 @@ const TopBar = () => {
         </Box>
         <Box display="flex"> 
             <IconButton onClick={colorMode.toggleColorMode}>
-            {theme.palette.mode === 'dark' ?(<DarkModeOutlinedIcon/>):(<LightModeOutlinedIcon/>)}
+                 {theme.palette.mode === 'dark' ?(<DarkModeOutlinedIcon/>):(<LightModeOutlinedIcon/>)}
+            </IconButton>
+            <IconButton>
+                <NotificationsOutlinedIcon/>
             </IconButton>
             <IconButton>
 
+                <SettingsOutlinedIcon/>
+            <IconButton/>
+                <PersonOutlinedIcon/>
             </IconButton>
 
-            <IconButton>
-
-            </IconButton>
-            <IconButton>
-
-            </IconButton>
         </Box>
     </Box>
 }
